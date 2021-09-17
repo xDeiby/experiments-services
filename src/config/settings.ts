5658a8d5635307c9bf325ec4e6622d1f4b6eea9e
Deiby C <deiby.cabrera@alumnos.uv.cl>
2021-09-17 13:00:28 -0300

    settings to heroku

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -2,4 +2,5 @@ import dotenv from 'dotenv-safe';
 
 dotenv.config();
 
-export const { PORT, MONGODB_URI } = process.env;
+export const PORT = process.env.PORT || 3001;
+export const { MONGODB_URI } = process.env;
