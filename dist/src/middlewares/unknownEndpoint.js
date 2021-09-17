00b2f63883ad76caf2c411f0d3d52e9ef64d0b89
Deiby Cabrera <55766812+xDeiby@users.noreply.github.com>
2021-09-17 18:11:20 -0300

    Delete dist/src directory

--- dist/src/middlewares/unknownEndpoint.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'Not Found' });
-};
-exports.default = unknownEndpoint;
